255973d58e1b6d21548dddfa73f24e8d590b19f2
Sarcaxtic12 <146767244+Sarcaxtic12@users.noreply.github.com>
2023-11-16 11:29:29 -0600

    Added P5js Temp

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// Major Project, F1 Racing
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX, mouseY, 100);
+}
