23057eef3cc6dc0ad5a56e02ac948a929155fb62
ChamberlandLyrist <146767709+ChamberlandLyrist@users.noreply.github.com>
2023-11-28 12:16:48 -0600

    started using p5.play

--- sketch.js
+++ sketch.js
@@ -1,53 +1,23 @@
-// Major Project, F1 Racing
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
-class Player{
-  constructor(name, x, y, color){
-    this.name = name;
-    this.x = x;
-    this.y = y;
-    this.color = color;
-    this.speed = 10;
-  }
 
-  move(){
-    if (keyIsDown(87)){
-      this.y -= this.speed;
-    }
-    if (keyIsDown(65)){
-      this.x -= this.speed;
-    }
-    if (keyIsDown(83)){
-      this.y += this.speed;
-    }
-    if (keyIsDown(68)){
-      this.x += this.speed;
-    }
-  }
 
-  display(){
-    noStroke();
-    fill(this.color);
-    // text(this.name, this.x, this.y);
-    circle(this.x, this.y, 69);
-  }
-}
 
-let p1;
+let cheese;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  p1 = new Player("lolsies", 100, 100, color(random(255), random(255), random(255)));
+  cheese = new Sprite();
+  cheese.width = 40;
+  cheese.height = 50;
+  world.gravity.y = -5;
 }
+
 
 function draw() {
-  background(220);
-  p1.move();
-  p1.display();  
+  // clear();
+  if (kb.pressing('down')){
+    cheese.move(30,"down", 3);
+  }
+  
+ 
 }
-
-// const smth = WebSocket("");
