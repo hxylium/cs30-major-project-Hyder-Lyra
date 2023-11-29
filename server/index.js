6994770df24561945c47a690cb3aebd6c04d0819
Sarcaxtic12 <146767244+Sarcaxtic12@users.noreply.github.com>
2023-11-29 12:18:17 -0600

    Client to Server communication established

--- server/index.js
+++ server/index.js
@@ -5,8 +5,15 @@ const wss = new WebSocket.Server({ port: 8082});
 wss.on("connection", ws => {
   console.log("New Client Connected!");
 
+  ws.on("message", data => {
+    console.log(`Client has sent us: ${data}`);
+
+    ws.send("smth in it");
+  });
+
   ws.on("close", () => {
     console.log("Client Disconnected!");
   });
 });
+
 
