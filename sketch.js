31c7ff3a3075a78efd6f173f5ad8f9d23064acd5
ChamberlandLyrist <146767709+ChamberlandLyrist@users.noreply.github.com>
2023-11-30 12:50:49 -0600

    mostly working now, working on trun while moving but not accelerating

--- sketch.js
+++ sketch.js
@@ -7,75 +7,69 @@ let cheese;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cheese = new Sprite();
-  cheese.rotation = 0;
-  cheese.accel = 2;
-  cheese.drag = 2;
+  cheese.h = 40;
+  cheese.w = 80;
+  // cheese.rotation = 0;
+  cheese.offset.x = 30;
+  cheese.move = 0;
+  cheese.turn = 0;
+  cheese.drag = 1;
   cheese.rotationDrag = 2;
+  cheese.rotationSpeed = 0;
   // cheese.w = 40;
   // cheese.h = 50;
   // cheese.rotation = 0;
-  // world.gravity.y = -5;
+  // world.gravity.y = 2;
 }
 
 
 function draw() {
   clear();
   
-  if (kb.pressing('left')){
-    cheese.rotationSpeed = 2;
+  
+  cheese.turn -= toZero(cheese.turn);
+  // cheese.move = 0;
+  cheese.turn = 0;
+  // map(Math.abs(cheese.vel.x)+Math.abs(cheese.vel.y),0,20,0,3)
+  if (Math.abs(cheese.vel.x)+Math.abs(cheese.vel.y) > 0){
+    if (kb.pressing('left')){
+      cheese.turn -= 1;
+    }
+    if (kb.pressing('right')){
+      cheese.turn += 1;
+    }
   }
-  if (kb.pressing('right')){
-    cheese.rotationSpeed = -2;
-  }
-  // cheese.bearing = -1*cheese.rotation;
-  cheese.bearing = -90;
+  cheese.rotationSpeed = cheese.turn;
 
-  // else {
-  //   cheese.rotationSpeed -= 
-  // }
-  if (kb.pressing('up')){
-    // console.log(Math.sin(cheese.rotation));
-    // cheese.vel.y += cheese.accel*Math.sin(cheese.bearing);
-    // cheese.vel.x += cheese.accel*Math.cos(cheese.rotation);
-    cheese.applyForce(5);
+
+  cheese.bearing = cheese.rotation-360;
+
+  if (keyIsDown(87)){
+    cheese.move += 20;
   }
-  // else{
-  //   cheese.vel.y =0;
-  //   cheese.vel.x = 0;
-  // }
   if (kb.pressing('space')){
-    // cheese.vel.y -= cheese.accel/2*Math.sin(cheese.rotation);
-    // cheese.vel.x -= cheese.accel/2*Math.cos(cheese.rotation);
-    cheese.applyForce(-5);
+    cheese.move -= 10;
   }
   
-  if (cheese.vel.y !== 0){
-    cheese.vel.y -= Math.abs(cheese.vel.y)/cheese.vel.y*cheese.accel/4;
+  cheese.applyForce(cheese.move);
+  if (keyIsDown(87)||kb.pressing('space')){
+    cheese.move =0;
   }
-  if (cheese.vel.x !== 0){
-    cheese.vel.x -= Math.abs(cheese.vel.x)/cheese.vel.x*cheese.accel/4;
+  else {
+    cheese.move -= toZero(cheese.move);
   }
-  if (cheese.vel.x + cheese.vel.y >= 5){
-    let dif = (cheese.vel.x + cheese.vel.y)-6;
-    let percent = dif/(cheese.vel.x + cheese.vel.y)
-    cheese.vel.y = cheese.vel.y/percent;
-    cheese.vel.x = cheese.vel.x/percent;
-  }
-  // console.log(cheese.x, cheese.y);
-  // cheese.vel.y*7/8
-  
 }
 
-// function ()
+function toZero(number){
+  return number/Math.abs(number);
+}
 
 class Car{
   contructor(){
     this.body = new Sprite();
     this.body.w = 40;
     this.body.h = 50;
-    this.body.rotation = 0;
-    this.carface = {x:0, y:0};
-    this.spd = 5;
+    
   }
 
   // move(){
