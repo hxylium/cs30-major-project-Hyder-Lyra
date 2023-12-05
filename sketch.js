4e1e37ec75fe8e3bf51c51d8fd982cd2cd15d13a
ChamberlandLyrist <146767709+ChamberlandLyrist@users.noreply.github.com>
2023-12-05 12:18:16 -0600

    tow "truck" experiment + fixed stupid mistake

--- sketch.js
+++ sketch.js
@@ -5,14 +5,16 @@
 let cheese;
 let ratio;
 
+let cheesewheel;
+let stringcheese;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ratio = smallest();
+  cheesewheel = new Sprite(100,0);
+  cheesewheel.r = 20;
   cheese = new Car();
-  // cheese.w = 40;
-  // cheese.h = 50;
-  // cheese.rotation = 0;
-  // world.gravity.y = 2;
+  stringcheese = new DistanceJoint(cheese.body, cheesewheel);
 }
 
 
@@ -20,24 +22,82 @@ function draw() {
   // clear();
   
   cheese.drive();
-  cheese.display();
+  // cheese.display();
+
+  // drive(mozarella);
+}
+
+let turn = 0;
+let move = 0;
+function drive(car){
+  if (Math.abs(car.vel.x)+Math.abs(car.vel.y) > 0){
+    if((!keyIsDown(16)||(Math.abs(car.vel.x)+Math.abs(car.vel.y) > 1.5))){
+      if (keyIsDown(65)){
+        turn -= 1;
+      // cheese.bearing = cheese.bare-90;
+      // cheese.applyForce(1);
+      }
+      if (keyIsDown(68)){
+        turn += 1;
+      // cheese.bearing = cheese.bare+90;
+      // cheese.applyForce(1);
+      }
+    // cheese.turn += toZero(cheese.turn)*;
+    }
+  // else {
+  //   console.log("handbrake stopped turning");
+  // }
+  }
+  car.rotationSpeed = turn;
+
+  // cheese.bearing = cheese.rotation-360;
+  // gas
+  if (keyIsDown(87)){
+    move += 10;
+  }
+  // brake
+  if (keyIsDown(32)){
+    move -= toZero(move)*2;
+    move -= 5;
+
+  }
+  // handbrake
+  if (keyIsDown(16)){
+    console.log("handbrake!!");
+    for (let i = 0; i < 1; i++){
+      car.speed -= toZero(car.speed)*10**-1;
+    }
+  }
+
+  car.bearing = car.rotation-360;
+
+  car.applyForce(move);
+  if (keyIsDown(87)||keyIsDown(32)){
+    move =0;
+  }
+  else {
+  // cheese.move -= toZero(cheese.move)*10**-6;
+  }
+
+  turn -= toZero(turn)*10**0;
+// if (keyIsDown(16)){
+//   cheese.turn -= toZero(cheese.turn)*10**3;
+// }
 }
 
 
-
 class Car{
-  contructor(){
+  constructor(){
     this.body = new Sprite();
-    console.log(this.body);
-    this.body.w = 20;
-    this.body.h = 40;
+    this.body.w = 40;
+    this.body.h = 20;
     this.body.drag = 2;
     this.body.rotationDrag =2;
     // this.body.rotationSpeed = 0;
     // this.body.vel = {x:0, y:0};
     this.move = 0;
     this.turn = 0;
-    this.colour = "red";
+    // this.coloour = "red";
   }
 
   drive() {
@@ -45,7 +105,6 @@ class Car{
   // cheese.turn = 0;
   // cheese.bare = cheese.rotation-360;
   // map(Math.abs(cheese.vel.x)+Math.abs(cheese.vel.y),0,20,0,3)
-  console.log(this.body);
     if (Math.abs(this.body.vel.x)+Math.abs(this.body.vel.y) > 0){
       if((!keyIsDown(16)||(Math.abs(this.body.vel.x)+Math.abs(this.body.vel.y) > 1.5))){
         if (keyIsDown(65)){
@@ -66,7 +125,7 @@ class Car{
     }
     this.body.rotationSpeed = this.turn;
 
-    // cheese.bearing = cheese.rotation-360;
+
     // gas
     if (keyIsDown(87)){
       this.move += 10;
@@ -84,7 +143,7 @@ class Car{
       }
     }
 
-    this.bearing = this.rotation-360;
+    this.body.bearing = this.body.rotation-360;
   
     this.body.applyForce(this.move);
     if (keyIsDown(87)||keyIsDown(32)){
@@ -100,19 +159,19 @@ class Car{
   // }
   }
 
-  display(){
-    fill(this.colour);
-    noStroke();
+  // display(){
+  //   fill(this.coolour);
+  //   noStroke();
     
-    push();
-    translate(this.body.x*ratio,this.body.y*ratio);
-    rotate(this.angle);
-    rectMode(CENTER);
-    rect(0,0,this.length*ratio,this.width*ratio);
-    pop();
-    // fill("black");
-    // circle(this.body.x*ratio+this.carface.x*3.5, this.body.y*ratio+this.carface.y*3.5, this.length/this.width*5);
-  }
+  //   push();
+  //   translate(this.body.x*ratio,this.body.y*ratio);
+  //   rotate(this.angle);
+  //   rectMode(CENTER);
+  //   rect(0,0,this.length*ratio,this.width*ratio);
+  //   pop();
+  //   // fill("black");
+  //   // circle(this.body.x*ratio+this.carface.x*3.5, this.body.y*ratio+this.carface.y*3.5, this.length/this.width*5);
+  // }
 }
 
 function toZero(number){
