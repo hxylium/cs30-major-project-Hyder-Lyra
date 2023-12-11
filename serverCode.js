53f1918d603627958fcbe0c128f168af863e230c
Sarcaxtic12 <146767244+Sarcaxtic12@users.noreply.github.com>
2023-12-11 12:18:20 -0600

    Trying client to client communication and rooms

---
+++ serverCode.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+let pos = { x: 0, y: 0 };
+
+function preload() {
+  // connect to a p5party server
+  partyConnect(
+    "wss://demoserver.p5party.org",
+    "Testing_F1",
+    // `${random(0, 100)}`
+    "Room1",
+    connected_f1()
+  );
+  
+  // tell p5.party to sync the pos object
+  pos = partyLoadShared("pos", pos);
+}
--- sketch.js
+++ sketch.js
@@ -1,19 +1,4 @@
-let pos = { x: 0, y: 0 };
-
-function preload() {
-  // connect to a p5party server
-  partyConnect(
-    "wss://demoserver.p5party.org",
-    "Testing_F1",
-    // `${random(0, 100)}`
-    "Room1",
-    connected_f1()
-  );
-  
-  // tell p5.party to sync the pos object
-  pos = partyLoadShared("pos", pos);
-}
-
+/* eslint-disable no-undef */
 function setup() {
   createCanvas(400, 400);
 }
