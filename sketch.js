db3bb4bc9c06cf26a0cae4c380408e90a64be9e9
Hyder Shahzaib Ahmed <axhyder@gmail.com>
2024-01-10 12:04:13 -0600

    Finally got multiplayer working real time

--- sketch.js
+++ sketch.js
@@ -4,21 +4,66 @@ function preload() {
   partyConnect("wss://demoserver.p5party.org", "something");
   shared = partyLoadShared("shared", { players: []});
   my = partyLoadMyShared();
-  guests = partyLoadGuestsShareds();
+  guests = partyLoadGuestShareds();
 }
 
 function setup() {
   createCanvas(500, 400);
-  my.ball = { x: 0, y: 0, r: 20 };
-
-  paartySubscribe("createBall", createBall);
+  my.ball = { x: 0, y: 0, r: 20, colour: color(random(255), random(255), random(255)) };
 }
 
 function draw() {
   moveBall();
-  if (partyIsHost()) {
-    drawScene();
+  drawScene();
+}
+
+function moveBall() {
+  if (keyIsDown(87)) {
+    my.ball.y -= 5;
   }
+
+  if (keyIsDown(83)) {
+    my.ball.y += 5;
+  }
+
+  if (keyIsDown(65)) {
+    my.ball.x -= 5;
+  }
+
+  if (keyIsDown(68)) {
+    my.ball.x += 5;
+  }
+}
+
+function keyPressed() {
+  if (key === " ") {
+    partyEmit("createBall", {x: random(width), y: random(height), r: 20,colour: color(random(255), random(255), random(255)) });
+  }
+
+  else {
+    return false;
+  }
+}
+
+function drawScene(){
+  background(0);
+  fill(my.ball.colour);
+  circle(my.ball.x, my.ball.y, my.ball.r * 2);
+  for (const guest of guests) {
+    fill(guest.ball.colour);
+    circle(guest.ball.x, guest.ball.y, guest.ball.r * 2);
+  }
+}
+
+function drawBall(ball) {
+  circle(ball.x, ball.y, ball.r * 2);
+}
+
+// window.addEventListener("keydown", function (e) {
+//   if (e.keyCode == 32 && e) {
+//     e.preventDefault();
+//   }
+// });
 
 // class Rect {
 //   constructor(l = 0, t = 0, w = 0, h = 0) {
